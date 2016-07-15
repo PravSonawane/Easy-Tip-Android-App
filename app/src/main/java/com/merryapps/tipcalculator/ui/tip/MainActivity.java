5405751e001eab21eeadc0f15548e81af88d8a25
PravinSonawane <june.pravin@gmail.com>
2016-07-14 23:15:24 -0400

    Redesigned UI. Added Fab. Added dialog on Fab

--- app/src/main/java/com/merryapps/tipcalculator/ui/tip/MainActivity.java
+++ app/src/main/java/com/merryapps/tipcalculator/ui/tip/MainActivity.java
@@ -1,8 +1,11 @@
 package com.merryapps.tipcalculator.ui.tip;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     //TODO add share icon in actionbar
     //TODO refactor split to numberOfPeople
 
+    private FloatingActionButton settingsFab;
+
     private static final String TAG = "MainActivity";
 
     private TipUiHandler tipUiHandler;
@@ -29,16 +34,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         initUiVariables();
-
-        tipUiHandler = new TipUiHandler();
-
     }
 
     private void initUiVariables() {
         Log.i(TAG, "initUiVariables: called");
         this.billAmountEdtTxt = (EditText) findViewById(R.id.activity_main_edtTxt_bill_amount_id);
+
+        initViews();
+    }
+
+    private void initViews() {
+        settingsFab = (FloatingActionButton) findViewById(R.id.fragment_main_settings_fab);
+        settingsFab.setOnClickListener(newSettingsFabOnClickListener());
+    }
+
+    private View.OnClickListener newSettingsFabOnClickListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SettingsDialogFragment dialogFragment = new SettingsDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "SettingsDialogFragment");
+            }
+        };
     }
 
 }
