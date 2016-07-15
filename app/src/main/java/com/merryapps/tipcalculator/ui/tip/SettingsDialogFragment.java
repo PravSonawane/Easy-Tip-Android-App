93f71e6ba1a86f703e4af915b2e54dad988d1e27
PravinSonawane <june.pravin@gmail.com>
2016-07-15 00:43:05 -0400

    Created custom dialog layout. Deleted old now unnecessary files

--- app/src/main/java/com/merryapps/tipcalculator/ui/tip/SettingsDialogFragment.java
+++ app/src/main/java/com/merryapps/tipcalculator/ui/tip/SettingsDialogFragment.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
+import com.merryapps.tipcalculator.R;
+
 /**
  * Created by mephisto on 7/14/16.
  */
@@ -16,13 +18,14 @@ public class SettingsDialogFragment extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Hello")
-                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        builder.setView(R.layout.fragment_settings_dialog)
+                .setTitle("Settings")
+                .setPositiveButton("DONE", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // FIRE ZE MISSILES!
                     }
                 })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // User cancelled the dialog
                     }
