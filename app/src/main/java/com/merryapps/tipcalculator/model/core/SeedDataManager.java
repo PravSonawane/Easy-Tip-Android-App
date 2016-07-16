0681e80d3909a6520daf87c827d56820370b6842
PravinSonawane <june.pravin@gmail.com>
2016-07-16 17:22:59 -0400

    Added more quotes

--- app/src/main/java/com/merryapps/tipcalculator/model/core/SeedDataManager.java
+++ app/src/main/java/com/merryapps/tipcalculator/model/core/SeedDataManager.java
@@ -25,6 +25,33 @@ public class SeedDataManager {
                 "Ayrton Senna", EntityState.LOCAL));
         quoteEntityDao.insertOrReplace(new QuoteEntity("A little thought and a little kindness are often worth more than a great deal of money.",
                 "John Ruskin", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("Let us not be satisfied with just giving money. Money is not enough, money can be got, but they need your hearts to love them. So, spread your love everywhere you go.",
+                "Mother Teresa", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("Real riches are the riches possessed inside.",
+                "B.C. Forbes", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("Wealth is the ability to fully experience life.",
+                "Henry David Thoreau", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("The hardest thing to understand in the world is the income tax.",
+                "Albert Einstein", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("Money can't buy love, but it improves your bargaining position.",
+                "Christopher Marlowe", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("It is not the creation of wealth that is wrong, but the love of money for its own sake.",
+                "Margaret Thatcher", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("When I was young I thought that money was the most important thing in life; now that I am old I know that it is.",
+                "Oscar Wilde", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("Many folks think they aren't good at earning money, when what they don't know is how to use it.",
+                "Frank A. Clark", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("If women didn't exist, all the money in the world would have no meaning.",
+                "Aristotle Onassis", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("The safe way to double your money is to fold it over once and put it in your pocket.",
+                "Kin Hubbard", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("It's morally wrong to allow a sucker to keep his money.",
+                "W. C. Fields", EntityState.LOCAL));
+        quoteEntityDao.insertOrReplace(new QuoteEntity("So you think that money is the root of all evil. Have you ever asked what is the root of all money?",
+                "Ayn Rand", EntityState.LOCAL));
+
+
+
 
         Log.d(TAG, "initializeDb: Inserting InvitationCategory objects - done");
 
